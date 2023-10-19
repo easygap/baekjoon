0075b46e81b701caac8191ee4c98fe9748faf1e5
apf_temp_admin <apf_temp_admin@21-manic-012>
2023-10-19 17:46:20 +0900

    2023 10 19 COMMIT!

---
+++ Baekjoon/src/q10809/Main.java
@@ -0,0 +1,21 @@
+package q10809;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] abc = new int[26];
+		
+		String word = sc.next();
+		char[] re = word.toCharArray();
+		
+		for(int i = 0; i < abc.length; i++) {
+			abc[i] = -1;
+		}
+		
+		
+	}
+}
